1cdeb8734c2d086b2f883037233994370b34ac3a
raju-anusuri <rajuanusuri639@gmail.com>
2026-01-06 23:08:13 +0530

    updated content

---
+++ climatefeature.java
@@ -0,0 +1,1 @@
+working area means files created, modifying and updating will be available in working area
--- comfort.java
+++ comfort.java
@@ -2,3 +2,5 @@ Git init going to create 3 logical areas
 1.Working area
 2.Staging area
 3.Local repository
+
+directly add and commit the at a time
